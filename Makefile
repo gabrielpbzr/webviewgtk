112f9f967ee1c5617b7c131fa436a2f4ef3579ad
Gabriel Parreira Bezerra <gabriel.bezerra@ufv.br>
2019-08-02 11:06:52 -0300

    Corrige o carregamento da biblioteca webkit2gtk e adiciona a webkitview ao programa

--- Makefile
+++ Makefile
@@ -1,8 +1,8 @@
 CC=gcc
 SOURCES=src/*.c
 INCLUDES= 
-LIBS= `pkg-config --libs gtk+-2.0`
-CFLAGS= -o $(OUTPUT) -Wall -g  `pkg-config --cflags gtk+-2.0`
+LIBS= `pkg-config --libs gtk+-3.0 webkit2gtk-4.0`
+CFLAGS= -o $(OUTPUT) -Wall -g  `pkg-config --cflags gtk+-3.0 webkit2gtk-4.0`
 OUTPUT=bin/webview
 
 all: clean build
--- src/main.c
+++ src/main.c
@@ -1,10 +1,45 @@
 #include<stdlib.h>
 #include <gtk/gtk.h>
+#include <webkit2/webkit2.h>
+
+void destroyWindowCallback(GtkWidget* widget, GtkWidget* window)
+{
+    gtk_main_quit();
+}
+
+static gboolean closeWebViewCallback(WebKitWebView* webView, GtkWidget* window)
+{
+    gtk_widget_destroy(window);
+    
+    return TRUE;
+}
+
 int main(int argc, char** argv)
 {
     gtk_init(&argc, &argv);
-    GtkWidget *win = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-    gtk_widget_show_all(win);
+    
+    GtkWidget *mainWindow = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    gtk_window_set_default_size(GTK_WINDOW(mainWindow), 800, 600);
+    
+    WebKitWebView* webView = WEBKIT_WEB_VIEW(webkit_web_view_new());
+
+    // Adiciona um container na janela principal para adicionar o webview
+    gtk_container_add(GTK_CONTAINER(mainWindow), GTK_WIDGET(webView));
+    
+    // Conecta o sinal de destruição da janela ativa para encerrar o programa
+    g_signal_connect(mainWindow, "destroy", G_CALLBACK(destroyWindowCallback), NULL);
+    
+    // Conecta o sinal de fechar o webview para destruir a janela ativa
+    g_signal_connect(webView, "close", G_CALLBACK(closeWebViewCallback), mainWindow);
+    
+    // Carrega a página passada como parametro para o programa
+    webkit_web_view_load_uri(webView, argv[1]);
+    
+    // Leva o foco direto para o webview
+    gtk_widget_grab_focus(GTK_WIDGET(webView));
+    
+    gtk_widget_show_all(mainWindow);
+    
     gtk_main();
     
     return 0;
